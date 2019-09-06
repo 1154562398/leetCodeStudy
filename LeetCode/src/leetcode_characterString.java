f0bc5e74179a44c57c09bfd43763aea2d59897fd
1154562398 <49087268+1154562398@users.noreply.github.com>
2019-09-06 17:22:54 +0800

    day3

--- LeetCode/src/leetcode_characterString.java
+++ LeetCode/src/leetcode_characterString.java
@@ -1,3 +1,7 @@
+import sun.plugin.javascript.navig.LinkArray;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -449,6 +453,302 @@ public class leetcode_characterString {
         return -1;
 
 
+
+    }
+
+    /****************【题目】**************
+     *
+     * 实现 strStr() 函数。
+     *
+     * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+     *
+     * 示例 1:
+     *
+     * 输入: haystack = "hello", needle = "ll"
+     * 输出: 2
+     * 示例 2:
+     *
+     * 输入: haystack = "aaaaa", needle = "bba"
+     * 输出: -1
+     * 说明:
+     *
+     * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+     *
+     * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+     *
+     *
+     */
+
+    public int strStr1(String haystack, String needle) {
+        if (needle == null || "".equals(needle)) {
+            return 0;
+        }
+        if (haystack == null || "".equals(haystack)) {
+            return -1;
+        }
+        return haystack.indexOf(needle);
+
+        /****************【 method 1 】**************
+         *
+         * KMP算法
+         *
+         * https://subetter.com/algorithm/kmp-algorithm.html
+         *
+         *
+         * if(needle.length()==0)
+         * return 0;
+         * int[] next=getNext(needle.toCharArray());
+         * int i=0,j=0;
+         * while(i<haystack.length()&&j<needle.length()){
+         * if(j==-1||haystack.charAt(i)==needle.charAt(j)){
+         *     i++;j++;
+         *  }else
+         *     j=next[j];
+         *  }
+         *  if(j==needle.length())
+         *     return i-j;
+         *  else
+         *     return -1;
+         *
+         *
+         *
+         * public int[] getNext(char[] pstr){
+         *    int[] next=new int[pstr.length+1];
+         *    int i=0,j=-1;
+         *    next[0]=-1;
+         *    while(i<pstr.length){
+         *    if(j==-1||pstr[i]==pstr[j]){
+         *        i++;
+         *        j++;
+         *     next[i]=j;
+         *     }else
+         *        j=next[j];
+         *    }
+         *    return next;
+         *
+         */
+    }
+
+
+    /****************【题目】**************
+     *
+     * 报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
+     *
+     * 1.     1
+     * 2.     11
+     * 3.     21
+     * 4.     1211
+     * 5.     111221
+     * 1 被读作  "one 1"  ("一个一") , 即 11。
+     * 11 被读作 "two 1s" ("两个一"）, 即 21。
+     * 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+     *
+     * 给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
+     *
+     * 注意：整数顺序将表示为一个字符串。
+     *
+     *
+     * 解释：下一个数是对上一个数的描述，比方说 1211 里有 “ 1 个 1 ， 1 个 2 ， 2 个 1 ” ，那么 111221 就是它的下一个数。通常我们把这个数列叫做“外观数列”。
+     */
+
+    public String countAndSay(int n) {
+        String str="1";
+        if(n==1)
+        {
+            return "1";
+        }
+
+        for(int i=1;i<n;i++)
+        {
+            StringBuilder sb=new StringBuilder();
+            char start=str.charAt(0);
+            int count=1;
+            for(int j=1;j<str.length();j++)
+            {
+                if(str.charAt(j)==start)
+                {
+                    count++;
+                }
+                else{
+                    sb.append(count).append(start);
+                    count=1;
+                    start=str.charAt(j);
+
+                }
+            }
+            sb.append(count).append(start);
+            str=sb.toString();
+        }
+        return str;
+
+    }
+
+    /****************【题目】**************
+     *
+     * 编写一个函数来查找字符串数组中的最长公共前缀。
+     *
+     * 如果不存在公共前缀，返回空字符串 ""。
+     *
+     * 示例 1:
+     *
+     * 输入: ["flower","flow","flight"]
+     * 输出: "fl"
+     * 示例 2:
+     *
+     * 输入: ["dog","racecar","car"]
+     * 输出: ""
+     * 解释: 输入不存在公共前缀。
+     * 说明:
+     *
+     * 所有输入只包含小写字母 a-z 。
+     *
+     *
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0||strs[0].equals(""))
+        {
+            return "";
+        }
+        else if(strs.length==1)
+        {
+            return strs[0];
+        }
+
+
+        StringBuilder sb=new StringBuilder();
+        for (int i=0;i<strs[0].length();i++)
+        {
+            for(int j=1;j< strs.length;j++)
+            {
+                if(strs[j].equals(""))
+                {
+                    return "";
+                }
+                if(i>=strs[j].length()||strs[0].charAt(i)!=strs[j].charAt(i))
+                {
+                    return sb.toString();
+                }
+            }
+            sb.append(strs[0].charAt(i));
+
+        }
+        return  sb.toString();
+
+
+    /****************【 method 1 】**************
+     *
+     * if(strs.length==0||strs[0].equals(""))
+     * {
+     *   return "";
+     * }
+     * else if(strs.length==1)
+     * {
+     *   return strs[0];
+     * }
+     * String str=strs[0];
+     * for(int j=1;j< strs.length;j++)
+     * {
+     *   while(!strs[j].startsWith(str))
+     *      {
+     *        str=str.substring(0,str.length()-1);
+     *      }
+     *   if(str.length()==0)
+     *      {
+     *        return "";
+     *      }
+     *
+     * }
+     * return str;
+     *
+     */
+
+
+
+
+    }
+
+    /****************【题目】**************
+     *
+     * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+     *
+     * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+     *
+     *      4->5->1->9
+     *
+     * 示例 1:
+     *
+     * 输入: head = [4,5,1,9], node = 5
+     * 输出: [4,1,9]
+     * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+     * 示例 2:
+     *
+     * 输入: head = [4,5,1,9], node = 1
+     * 输出: [4,5,9]
+     * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+     *
+     *
+     * 说明:
+     *
+     * 链表至少包含两个节点。
+     * 链表中所有节点的值都是唯一的。
+     * 给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+     * 不要从你的函数中返回任何结果。
+     *
+     */
+
+
+
+    public class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+     }
+    public void deleteNode(ListNode node) {
+        if(node.next!=null){
+
+            node.val=node.next.val;
+
+            node.next=node.next.next;
+
+        }
+
+
+    }
+
+
+    /****************【题目】**************
+     *
+     * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+     *
+     * 示例：
+     *
+     * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+     *
+     * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+     * 说明：
+     *
+     * 给定的 n 保证是有效的。
+     *
+     * 进阶：
+     *
+     * 你能尝试使用一趟扫描实现吗？
+     *
+     */
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode node=new ListNode(0);
+        ListNode node2=new ListNode(0);
+
+        for(int i=0;i<n;i++)
+        {
+            node=node.next;
+        }
+        while(node.next!=null)
+        {
+            node=node.next;
+            node2=node2.next
+        }
+        System.out.println(node.val);
 
     }
 
